939a8de8ad4ced27068280380db6f6d83f76aff1
SimonMansoury <saymon3228@gmail.com>
2021-03-25 08:18:39 +0200

    lab 2

--- App.tsx
+++ App.tsx
@@ -1,19 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {Alert, Button, StyleSheet, Text, View} from 'react-native';
+import SelectMultiple from 'react-native-select-multiple'
+
+const fruits = ['Apples', 'Oranges', 'Pears', 'Milk', 'Salo', 'Hleb', 'Kefir']
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    const [selectedItems, setSelectedItems] = useState([]);
+    const [isVisible, setIsVisible] = useState(false);
+
+    const onCheckBoxChange = useCallback((items) => {
+        setSelectedItems(items)
+    }, [setSelectedItems]);
+
+    const toggleOutput = useCallback(() => {
+        setIsVisible(!isVisible);
+
+        if (selectedItems.length == 0) {
+            Alert.alert('NO PRODUCTS IN CART');
+            return;
+        } else {
+        }
+        console.log(selectedItems)
+
+    }, [setIsVisible, isVisible]);
+
+    return (
+        <View style={{width: '100%', marginTop: 60, marginHorizontal: 10}}>
+            {isVisible ? (
+                <View>
+                    <Text>{selectedItems.map((item) => {
+                        return item.label;
+                    }).join(', ')}</Text>
+                    <Button title='BACK' onPress={toggleOutput}/>
+                </View>
+            ) : (
+                <View>
+                    <Text>Choose products please</Text>
+                    <View style={{}}>
+                        <SelectMultiple
+                            items={fruits}
+                            selectedItems={selectedItems}
+                            onSelectionsChange={onCheckBoxChange}/>
+                    </View>
+                    <Button title='OK' onPress={toggleOutput}/>
+                </View>
+            )}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    btn: {
+        backgroundColor: 'red'
+    }
 });
+
+//
+// const styles = StyleSheet.create({
+//     container: {
+//         alignSelf: 'stretch',
+//         width: '100pt',
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+//     multiselect: {
+//         maxHeight: '70%',
+//         width: '100%',
+//         alignSelf: 'stretch',
+//     },
+// });
