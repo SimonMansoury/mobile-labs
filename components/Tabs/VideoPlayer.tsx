16504214c2fe9709b6791dace68b1ab3581efa00
SimonMansoury <saymon3228@gmail.com>
2021-04-21 17:45:19 +0300

    4 lab v2

--- components/Tabs/VideoPlayer.tsx
+++ components/Tabs/VideoPlayer.tsx
@@ -1,102 +1,138 @@
-import React, { useState, useRef } from 'react';
-import { StyleSheet, View, Platform } from 'react-native';
-import MediaControls, { PLAYER_STATES } from 'react-native-media-controls';
+// React Native Video Library to Play Video in Android and IOS
+// https://aboutreact.com/react-native-video/
+
+// import React in our code
+import React, {useState, useRef} from 'react';
+
+// import all the components we are going to use
+import {StyleSheet, Text, View} from 'react-native';
+
+//Import React Native Video to play video
 import Video from 'react-native-video';
 
+//Media Controls to control Play/Pause/Seek and full screen
+import
+    MediaControls, {PLAYER_STATES}
+    from 'react-native-media-controls';
+
 const VideoPlayer = () => {
-
-    // The video we will play on the player.
-    const video = require('../../videos/video.mp4')
     const videoPlayer = useRef(null);
-    const [duration, setDuration] = useState(0);
-    const [paused, setPaused] = useState(true);
-
     const [currentTime, setCurrentTime] = useState(0);
-    const [playerState, setPlayerState] = useState(PLAYER_STATES.PAUSED);
+    const [duration, setDuration] = useState(0);
+    const [isFullScreen, setIsFullScreen] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
+    const [paused, setPaused] = useState(false);
+    const [playerState, setPlayerState] = useState(PLAYER_STATES.PLAYING);
+    const [screenType, setScreenType] = useState('content');
 
     const onSeek = (seek) => {
-        videoPlayer?.current.seek(seek);
+        //Handler for change in seekbar
+        videoPlayer.current.seek(seek);
     };
 
-    const onSeeking = (currentVideoTime: number) => setCurrentTime(currentVideoTime);
-
-    const onPaused = (newState: PLAYER_STATES) => {
+    const onPaused = (playerState) => {
+        //Handler for Video Pause
         setPaused(!paused);
-        setPlayerState(newState);
+        setPlayerState(playerState);
     };
 
     const onReplay = () => {
-        videoPlayer?.current.seek(0);
-        setCurrentTime(0);
-        if (Platform.OS === 'android') {
-            setPlayerState(PLAYER_STATES.PAUSED);
-            setPaused(true);
-        } else {
-            setPlayerState(PLAYER_STATES.PLAYING);
-            setPaused(false);
-        }
+        //Handler for Replay
+        setPlayerState(PLAYER_STATES.PLAYING);
+        videoPlayer.current.seek(0);
     };
 
     const onProgress = (data) => {
-        if (!isLoading) {
+        // Video Player will progress continue even if it ends
+        if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
             setCurrentTime(data.currentTime);
         }
     };
 
     const onLoad = (data) => {
-        setDuration(Math.round(data.duration));
+        setDuration(data.duration);
         setIsLoading(false);
     };
 
-    const onLoadStart = () => setIsLoading(true);
+    const onEnd = () => setPlayerState(PLAYER_STATES.ENDED);
 
-    const onEnd = () => {
-        setPlayerState(PLAYER_STATES.ENDED);
-        setCurrentTime(duration);
+    const onError = () => alert('Oh! ', error);
+
+    const exitFullScreen = () => {
+        alert('Exit full screen');
     };
 
-    return (
+    const enterFullScreen = () => {
+    };
+
+    const onFullScreen = () => {
+        setIsFullScreen(isFullScreen);
+        if (screenType == 'content') setScreenType('cover');
+        else setScreenType('content');
+    };
+
+    const renderToolbar = () => (
         <View>
+            <Text style={styles.toolbar}> toolbar </Text>
+        </View>
+    );
+
+    const onSeeking = (currentTime) => setCurrentTime(currentTime);
+
+    return (
+        <View style={{flex: 1}}>
             <Video
+                source={{
+                    uri:
+                        'https://assets.mixkit.co/videos/download/mixkit-countryside-meadow-4075.mp4',
+                }}
                 onEnd={onEnd}
                 onLoad={onLoad}
-                onLoadStart={onLoadStart}
-                posterResizeMode={'cover'}
                 onProgress={onProgress}
+                onError={onError}
                 paused={paused}
-                ref={(ref) => (videoPlayer.current = ref)}
-                resizeMode={'cover'}
-                source={video}
-                style={styles.backgroundVideo}
+                ref={videoPlayer}
+                style={styles.mediaPlayer}
+                volume={10}
             />
+
             <MediaControls
-                isFullScreen={false}
                 duration={duration}
                 isLoading={isLoading}
-                progress={currentTime}
+                mainColor="#333"
+                onFullScreen={onFullScreen}
                 onPaused={onPaused}
                 onReplay={onReplay}
                 onSeek={onSeek}
                 onSeeking={onSeeking}
                 playerState={playerState}
-                sliderStyle={{ containerStyle: {}, thumbStyle: {}, trackStyle: {} }}
+                progress={currentTime}
+                containerStyle={styles.container}
+                toolbar={renderToolbar()}
             />
         </View>
-
     );
 };
 
+export default VideoPlayer;
+
 const styles = StyleSheet.create({
-    backgroundVideo: {
-        height: 250,
-        width: '100%',
-    },
-    mediaControls: {
-        height: '100%',
+    container: {
         flex: 1,
-        alignSelf: 'center',
+    },
+    toolbar: {
+        marginTop: 30,
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 5,
+    },
+    mediaPlayer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        backgroundColor: 'black',
+        justifyContent: 'center',
     },
 });
-
-export default VideoPlayer;
